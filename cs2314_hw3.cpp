f4d154de34a58106ab8af1957805fa2e1bab49b6
JuneyBoy <arjun.ganesan@ou.edu>
2020-09-30 17:31:43 -0500

    Implemented working selection sort

--- cs2314_hw3.cpp
+++ cs2314_hw3.cpp
@@ -3,10 +3,48 @@
 
 using namespace std;
 
-void swap(vector<int> &vect, int idx){
-    int temp = vect.at(idx);
-    vect.at(idx) = vect.at(idx+1);
-    vect.at(idx+1) = temp;
+void swap(vector<int> &vect, int idx1, int idx2){
+    int temp = vect.at(idx1);
+    vect.at(idx1) = vect.at(idx2);
+    vect.at(idx2) = temp;
+}
+
+void selectionSort(vector<int> &vect){
+    for(int i = 0; i < vect.size(); ++i){
+        int maxIdx = i;
+        for(int j = i + 1; j < vect.size(); ++j){
+            if(vect.at(j) > vect.at(maxIdx)){
+                maxIdx = j;
+            }
+        }
+        swap(vect, maxIdx, i);
+    }
+}
+
+vector<int> subVectorMaker(vector<int> &vect, int start, int end){
+    vector<int> subVect;
+
+    for(int i = start; i < end; ++i){
+        subVect.push_back(vect.at(i));
+    }
+    return subVect;
+}
+
+void mergeSort(vector<int> &vect){
+    int mid = vect.size()/2;
+    vector<int> leftVec = subVectorMaker(vect, 0, mid);
+    vector<int> rightVec = subVectorMaker(vect, mid, vect.size());
+
+    selectionSort(leftVec);
+    selectionSort(rightVec);
+
+    int leftTracker = 0;
+    int rightTracker = 0;
+
+    for(int i = 0; i < vect.size(); ++i){
+        
+    }
+
 }
 
 void bubbleSort(vector<int> &vect){
@@ -23,7 +61,7 @@ void bubbleSort(vector<int> &vect){
             int b = vect.at(j+1);
 
             if(a < b){
-                swap(vect, j);
+                swap(vect, j, j+1);
                 noSwaps = false;
             }
         }
@@ -56,10 +94,18 @@ int main(){
 
     cout << endl;
 
-    bubbleSort(list);
+    selectionSort(list);
 
     for(int i = 0; i < list.size(); ++i){
         cout << list.at(i) << " ";
+    }
+
+    cout << endl;
+
+    vector<int> sublist = subVectorMaker(list, 0, 4);
+
+    for(int i = 0; i < sublist.size(); ++i){
+        cout << sublist.at(i) << " ";
     }
 
     return 0;
